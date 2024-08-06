98cd770c47bcc9f4c153701a4cc6a61facb7042e
jihadhassan001 <167314484+jihadhassan001@users.noreply.github.com>
2024-08-06 16:04:20 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*example1*/
+function clickHere(){
+    document.getElementById('clickHere').innerHTML="my name is jihad";
+}
+function clickSee(){
+    document.getElementById('click').innerHTML="my name is jihad";
+}
+/*banner section start*/
+function clickSaw(){
+    document.getElementById('seeOne').innerHTML="my name is jihad hassan i will try";    
+}
+function clickSpace(){
+     document.getElementById('seeTwo').innerHTML="my name is jihad hassan i will try";   
+}
+function clickThis(){
+    document.getElementById('seeThree').innerHTML="my name is jihad hassan i will try";
+}
+function clickSeen(){
+    document.getElementById('seeFour').innerHTML="my name is jihad hassan i will try";
+}
